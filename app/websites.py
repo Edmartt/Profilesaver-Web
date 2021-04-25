8a98d667aed027c6ae0ff74166e97d455147a195
Sam Sepiol <samsepiolhack@gmail.com>
2021-04-25 01:53:26 -0500

    Se modifican los marcadores de parámetros en consultas debido al cambio de gestor de BD

--- app/websites.py
+++ app/websites.py
@@ -13,9 +13,9 @@ class Website:
 
     def saveweb(self,web):
         try:
-            cursor=get_db()
-            cursor.execute('INSERT INTO Websites (user_id,web_name,web_email,web_pass,nota,web_username) VALUES(?,?,?,?,?,?)',(session['user_id'],self.web_name,self.web_email,self.web_pass,self.nota,self.web_username))
-            cursor.commit()
+            db,cursor=get_db()
+            cursor.execute('INSERT INTO Websites (user_id,web_name,web_email,web_pass,nota,web_username) VALUES(%s,%s,%s,%s,%s,%s)',(self.user_id,self.web_name,self.web_email,self.web_pass,self.nota,self.web_username))
+            db.commit()
         except Exception as e:
             print(e)
         finally:
@@ -24,8 +24,8 @@ class Website:
     @staticmethod
     def showprofiles(user_id):
         try:
-            cursor=get_db().cursor()
-            cursor.execute('SELECT * FROM Websites WHERE user_id=?',(user_id,))
+            db,cursor=get_db()
+            cursor.execute('SELECT * FROM Websites WHERE user_id=%s',(user_id,))
             webs=cursor.fetchall()
             return webs
         except Exception as e:
@@ -34,19 +34,30 @@ class Website:
             close_db()
 
     def editWeb(self,web_id):
+        '''
+        Esta función escribe los cambios en la base de datos, una vez 
+        se han cargado los datos en el formulario presionando el botón edit
+
+        :param web_id: el id del sitio web o red social guardado
+        '''
         try:
-            close_db()
-            cursor=get_db()
-            cursor.execute('UPDATE Websites SET web_name=?,web_email=?,web_pass=?,nota=?,web_username=? WHERE web_id=?',(self.web_name,self.web_email,self.web_pass,self.nota,self.web_username,web_id))
-            cursor.commit()
+            db,cursor=get_db()
+            cursor.execute('UPDATE Websites SET web_name=%s,web_email=%s,web_pass=%s,nota=%s,web_username=%s WHERE web_id=%s',(self.web_name,self.web_email,self.web_pass,self.nota,self.web_username,web_id))
+            db.commit()
         except Exception as e:
             print(e)
 
     @staticmethod
-    def loadWeb(id):
+    def loadWeb(web_id):
+        '''
+        loadWeb carga todos los datos del sitio web seleccionado
+        en el index de la aplicación  a un formulario desde donde se
+        podrán editar
+        :param web_id: id del sitio web o red social guardado
+        '''
         try:
-            cursor=get_db().cursor()
-            cursor.execute('SELECT * FROM Websites WHERE web_id=?',(id,))
+            db,cursor=get_db()
+            cursor.execute('SELECT * FROM Websites WHERE web_id=%s',(id,))
             data=cursor.fetchone()
             return data
         except Exception as e:
@@ -57,10 +68,9 @@ class Website:
     @staticmethod
     def deleteWeb(id):
         try:
-            close_db()
-            cursor=get_db()
-            cursor.execute('DELETE FROM Websites WHERE web_id=?',(id,))
-            cursor.commit()
+            db,cursor=get_db()
+            cursor.execute('DELETE FROM Websites WHERE web_id=%s',(id,))
+            db.commit()
         except Exception as e:
             print(e)
         finally:
