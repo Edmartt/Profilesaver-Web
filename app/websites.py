490d9fcbcff8fab97327a33da226e572ac7f0eb4
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-18 17:37:13 -0500

    Fixed readability and still working on it.

--- app/websites.py
+++ app/websites.py
@@ -1,20 +1,21 @@
-from .database import get_db,close_db,g
-from flask import session
-from sqlite3 import DatabaseError
-class Website:
-    def __init__(self,web_username,web_name,web_email,web_pass,nota,user_id=None,id=None):
-        self.web_username=web_username
-        self.web_name=web_name
-        self.web_email=web_email
-        self.web_pass=web_pass
-        self.nota=nota
-        self.user_id=user_id
-        self.id=id
+from .database import get_db, close_db
 
-    def saveweb(self,web):
+
+class Website:
+    def __init__(self, web_username, web_name, web_email, web_pass, nota, user_id=None, id=None):
+        self.web_username = web_username
+        self.web_name = web_name
+        self.web_email = web_email
+        self.web_pass = web_pass
+        self.nota = nota
+        self.user_id = user_id
+        self.id = id
+
+    def saveweb(self, web):
         try:
-            db,cursor=get_db()
-            cursor.execute('INSERT INTO Websites (user_id,web_name,web_email,web_pass,nota,web_username) VALUES(%s,%s,%s,%s,%s,%s)',(self.user_id,self.web_name,self.web_email,self.web_pass,self.nota,self.web_username))
+            db, cursor = get_db()
+            cursor.execute('INSERT INTO Websites(user_id, web_name, web_email, web_pass, nota, web_username) VALUES(%s, %s, %s, %s, %s, %s)', 
+          (self.user_id, self.web_name, self.web_email, self.web_pass, self.nota, self.web_username))
             db.commit()
         except Exception as e:
             print(e)
@@ -24,25 +25,25 @@ class Website:
     @staticmethod
     def showprofiles(user_id):
         try:
-            db,cursor=get_db()
-            cursor.execute('SELECT * FROM Websites WHERE user_id=%s',(user_id,))
-            webs=cursor.fetchall()
+            db, cursor = get_db()
+            cursor.execute('SELECT * FROM Websites WHERE user_id=%s', (user_id,))
+            webs = cursor.fetchall()
             return webs
         except Exception as e:
             print(e)
         finally:
             close_db()
 
-    def editWeb(self,web_id):
+    def editWeb(self, web_id):
         '''
-        Esta función escribe los cambios en la base de datos, una vez 
+        Esta función escribe los cambios en la base de datos, una vez
         se han cargado los datos en el formulario presionando el botón edit
 
         :param web_id: el id del sitio web o red social guardado
         '''
         try:
-            db,cursor=get_db()
-            cursor.execute('UPDATE Websites SET web_name=%s,web_email=%s,web_pass=%s,nota=%s,web_username=%s WHERE web_id=%s',(self.web_name,self.web_email,self.web_pass,self.nota,self.web_username,web_id))
+            db, cursor = get_db()
+            cursor.execute('UPDATE Websites SET web_name=%s,web_email=%s,web_pass=%s,nota=%s,web_username=%s WHERE web_id=%s', (self.web_name, self.web_email, self.web_pass, self.nota, self.web_username, web_id))
             db.commit()
         except Exception as e:
             print(e)
@@ -56,9 +57,9 @@ class Website:
         :param web_id: id del sitio web o red social guardado
         '''
         try:
-            db,cursor=get_db()
-            cursor.execute('SELECT * FROM Websites WHERE web_id=%s',(id,))
-            data=cursor.fetchone()
+            db, cursor = get_db()
+            cursor.execute('SELECT * FROM Websites WHERE web_id=%s', (id,))
+            data = cursor.fetchone()
             return data
         except Exception as e:
             print(e)
@@ -68,8 +69,8 @@ class Website:
     @staticmethod
     def deleteWeb(id):
         try:
-            db,cursor=get_db()
-            cursor.execute('DELETE FROM Websites WHERE web_id=%s',(id,))
+            db, cursor = get_db()
+            cursor.execute('DELETE FROM Websites WHERE web_id=%s', (id,))
             db.commit()
         except Exception as e:
             print(e)
