41e707802d1e97949fb126899721cefe9da07631
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-18 17:15:25 -0500

    Fixed assignament blank spaces

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,10 @@
 import os
 from flask import Flask
-from .database import get_db as db
 from config import config
 
 
-
 def create_app(config_name):
-    app=Flask(__name__)
+    app = Flask(__name__)
     app.config.from_object(config[config_name])
     config[config_name].init_app(app)
 
@@ -19,6 +17,5 @@ def create_app(config_name):
 
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
-
 
     return app
