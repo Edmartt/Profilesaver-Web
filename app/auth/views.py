4b027d682c044839d1a8b3402680ab85f9ae43be
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-18 17:17:41 -0500

    deleted unused imports and reordered login_required place. Made code refactoring to load_logged_in_user with ternary operator. Changed login view, now it creates an instance from User class for using select_user method and validate login user data. The logout endpoint is now protected.

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,42 +1,10 @@
 import functools
-from flask import render_template,redirect,request,url_for,flash,session,g
-from markupsafe import escape
-from . import auth
-from .forms import Login,Register
+from flask import render_template, redirect,\
+        request, url_for, flash, session, g
 
 from app.users import User
-
-
-@auth.before_app_request
-def load_logged_in_user():
-    user_id=session.get('user_id')
-    if user_id is None:
-        g.user=None
-    else:
-        g.user=User.select_user_by_id(user_id)
-
-@auth.route('/login',methods=['GET','POST'])
-def login():
-    form=Login()
-    if form.validate_on_submit():
-        user=User.select_user(form.username.data)
-
-        if user is not None and user.verify_password(form.password.data):
-            session.clear()
-            session['user_id']=user.id
-            next=request.args.get('next',None)
-
-            if next is None or not next.startswith('/'):
-                next=url_for('main.index')
-            return redirect(next)
-
-        flash('Nombre de usuario o contraseña no válido')
-    return render_template('auth/login.html',form=form)
-
-@auth.route('/logout')
-def logout():
-    session.clear()
-    return redirect(url_for('main.index'))
+from . import auth
+from .forms import Login, Register
 
 
 def login_required(view):
@@ -44,24 +12,60 @@ def login_required(view):
     def wrapped_view(**kwargs):
         if g.user is None:
             flash('Debe iniciar sesión para ver esto')
-            #redirigiremos a login en cada intento de acceder a una vista
-            # que corresponda a data privada. la variable next, guarda la url que se quiso visitar
-            return redirect(url_for('auth.login',next=request.url_rule))
+            # redirigiremos a login en cada intento de acceder a una vista
+            # la variable next, guarda la url que se quiso visitar
+            return redirect(url_for('auth.login', next=request.url_rule))
         return view(**kwargs)
     return wrapped_view
 
-@auth.route('/register',methods=['GET','POST'])
+
+@auth.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    g.user = None if user_id is None else User.select_user_by_id(user_id)
+
+
+@auth.route('/login', methods=['GET', 'POST'])
+def login():
+    form = Login()
+
+    if form.validate_on_submit():
+        user = User(form.username.data, form.password.data)
+        user_data = user.select_user(form.username.data)
+
+        if user_data is not None and user.verify_password(form.password.data):
+            session.clear()
+            session['user_id'] = user_data['id']
+            next = request.args.get('next', None)
+
+            if next is None or not next.startswith('/'):
+                next = url_for('main.index')
+            return redirect(next)
+
+        flash('Nombre de usuario o contraseña no válido')
+    return render_template('auth/login.html', form=form)
+
+
+@auth.route('/logout')
+@login_required
+def logout():
+    session.clear()
+    return redirect(url_for('main.index'))
+
+
+@auth.route('/register', methods=['GET', 'POST'])
 def register():
-    form=Register()
+    form = Register()
 
     if session.get('user_id') is not None:
         return redirect(url_for('main.index'))
 
-    elif form.validate_on_submit():
-            user=User(form.username.data,form.email.data,form.password.data)
-            user.password=form.password.data
+    else:
+        if form.validate_on_submit():
+            user = User(form.username.data,
+                        form.email.data, form.password.data)
+            user.password = form.password.data
             user.registerUser(user)
             flash('Usuario Registrado')
             return redirect(url_for('auth.login'))
-    return render_template('auth/register.html',form=form)
-
+    return render_template('auth/register.html', form=form)
