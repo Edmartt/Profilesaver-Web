8546f35f900ff6952beb962e6e8b3b744357aadd
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-29 00:14:08 -0500

    Now run.py makes possible running unittest from command line

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
+import unittest
 from app import create_app
 import os
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
