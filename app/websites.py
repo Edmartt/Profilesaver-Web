23d336a729abe0d9fd638314a4256e49093c8347
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-31 23:16:50 -0500

    Added class attributes at the beginning of the class and return type for the methods

--- app/websites.py
+++ app/websites.py
@@ -1,9 +1,17 @@
 from flask import session
 from .database import get_db, close_db
+from .main.forms import AddAccount
 
 
 class Website:
-    def __init__(self, web_form):
+    web_username: str
+    web_name: str
+    web_email: str
+    web_pass: str
+    web_nota: str
+    user_id: int
+
+    def __init__(self, web_form: AddAccount):
         self.web_username = web_form.username.data
         self.web_name = web_form.url.data
         self.web_email = web_form.email.data
@@ -11,7 +19,7 @@ class Website:
         self.nota = web_form.notas.data
         self.user_id = session['user_id']
 
-    def saveweb(self):
+    def saveweb(self) -> None:
         connection, cursor = get_db()
         query = '''INSERT INTO Websites(user_id, web_name, web_email, web_pass,
         nota, web_username) VALUES(%s, %s, %s, %s, %s, %s)'''
@@ -25,8 +33,9 @@ class Website:
             print(ex)
         finally:
             close_db()
+        return
 
-    def showprofiles(self):
+    def showprofiles(self) -> dict:
         _, cursor = get_db()
         query = 'SELECT * FROM websites WHERE user_id=%s'
         try:
@@ -38,7 +47,7 @@ class Website:
         finally:
             close_db()
 
-    def editWeb(self, web_id):
+    def editWeb(self, web_id: int) -> None:
         connection, cursor = get_db()
         query = '''UPDATE Websites SET web_name = %s, web_email = %s,
         web_pass = %s, nota = %s, web_username = %s WHERE web_id = %s'''
@@ -52,9 +61,10 @@ class Website:
             print(ex)
         finally:
             close_db()
+        return
 
     @staticmethod
-    def loadWeb(web_id):
+    def loadWeb(web_id: int) -> dict:
         _, cursor = get_db()
         try:
             cursor.execute('SELECT * FROM Websites WHERE web_id=%s', (web_id,))
@@ -67,7 +77,7 @@ class Website:
             close_db()
 
     @staticmethod
-    def deleteWeb(id):
+    def deleteWeb(id: int) -> None:
         connection, cursor = get_db()
         query = 'DELETE FROM Websites WHERE web_id=%s'
         try:
@@ -77,3 +87,4 @@ class Website:
             print(ex)
         finally:
             close_db()
+        return
