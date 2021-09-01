a07670e1f986a146663c355868b64b9ac3ea9431
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-08-31 23:13:07 -0500

    Improves the readability of the class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,22 +1,37 @@
 from flask_wtf import FlaskForm
-from wtforms.validators import DataRequired,Length,Email,EqualTo,Regexp
-from wtforms import StringField,PasswordField,TextAreaField,SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
+from wtforms import StringField, PasswordField, TextAreaField, SubmitField
 from wtforms import ValidationError
 
+
 class AddAccount(FlaskForm):
-    username=StringField('Username',validators=[DataRequired()])
-    url=StringField('URL DEL SITIO',validators=[DataRequired(),Length(1,64)])
-    email=StringField('Email',validators=[DataRequired()])
-    password=PasswordField('Password',validators=[DataRequired(),EqualTo('passconfirm',message='El password no coincide'),Length(8,64)])
-    passconfirm=PasswordField('Confirmar Password',validators=[DataRequired(),Length(8,64)])
-    notas=TextAreaField('Anotaciones sobre este perfil',validators=[DataRequired()])
-    submit=SubmitField('Guardar')
+
+    username = StringField('Username', validators=[DataRequired()])
+    url = StringField('URL DEL SITIO', validators=[DataRequired(),
+                                                   Length(1, 64)])
+    email = StringField('Email', validators=[DataRequired()])
+
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo(
+        'passconfirm', message='El password no coincide'), Length(8, 64)])
+
+    passconfirm = PasswordField(
+            'Confirmar Password', validators=[DataRequired(), Length(8, 64)])
+
+    notas = TextAreaField(
+            'Anotaciones sobre este perfil', validators=[DataRequired()])
+
+    submit = SubmitField('Guardar')
+
 
 class EditWeb(FlaskForm):
-    username=StringField('Username',validators=[DataRequired()])
-    url=StringField('URL DEL SITIO',validators=[DataRequired(),Length(1,64)])
-    email=StringField('Email',validators=[DataRequired()])
-    password=StringField('Password',validators=[DataRequired()])
-    notas=TextAreaField('Anotaciones sobre este perfil',validators=[DataRequired()])
-    submit=SubmitField('Modificar')
 
+    username = StringField('Username', validators=[DataRequired()])
+
+    url = StringField('URL DEL SITIO',
+                      validators=[DataRequired(), Length(1, 64)])
+
+    email = StringField('Email', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()])
+    notas = TextAreaField('Anotaciones sobre este perfil',
+                          validators=[DataRequired()])
+    submit = SubmitField('Modificar')
