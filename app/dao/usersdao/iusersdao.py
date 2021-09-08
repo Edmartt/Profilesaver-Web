e2db81a9781335e861ca4f9e4bcb346bdfd9c26a
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-08 14:08:12 -0500

    Now all the methods in this class has a parameter, an object from Querymanager for handling sql querys

--- app/dao/usersdao/iusersdao.py
+++ app/dao/usersdao/iusersdao.py
@@ -1,17 +1,18 @@
 from abc import ABC, abstractmethod
 from app.users import User
+from app.querymanager import QueryManager
 
 
 class IUsersdao(ABC):
 
     @abstractmethod
-    def get(self, user: User) -> dict:
+    def get(self, user: User, db: QueryManager) -> dict:
         pass
 
     @abstractmethod
-    def add(self, user: User) -> None:
+    def add(self, user: User, db: QueryManager) -> None:
         pass
 
     @abstractmethod
-    def check_user_exists(self, user: User) -> bool:
+    def check_user_exists(self, user: User, db: QueryManager) -> bool:
         pass
