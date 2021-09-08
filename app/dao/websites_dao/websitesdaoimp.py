d1e7f2d5dfb41985604a1f81cc09fa02ee8654c0
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-08 14:12:55 -0500

    Now the methods of this class gets a QueryManager object for handling sql querys and avoid repeated code

--- app/dao/websites_dao/websitesdaoimp.py
+++ app/dao/websites_dao/websitesdaoimp.py
@@ -1,3 +1,4 @@
+from app.querymanager import QueryManager
 from .iwebsitesdao import IWebsitesdao
 from app.websites import Website
 from app.database import get_db, close_db
@@ -5,44 +6,26 @@ from app.database import get_db, close_db
 
 class Websitedao(IWebsitesdao):
 
-    def get(self, web_id: int) -> dict:
-        _, cursor = get_db()
-        try:
-            cursor.execute('SELECT * FROM Websites WHERE web_id=%s', (web_id,))
-            data = cursor.fetchone()
-            if data:
-                return data
-        except Exception as ex:
-            print(ex)
-        finally:
-            close_db()
+    def get(self, web_id: int, db: QueryManager) -> dict:
+        query = 'SELECT * FROM Websites WHERE web_id=%s'
+        data = db.select(query, web_id)
+        if data:
+            return data
 
-    def get_all(self, user_id: int) -> dict:
-        _, cursor = get_db()
+    def get_all(self, user_id: int, db: QueryManager) -> dict:
         query = 'SELECT * FROM websites WHERE user_id=%s'
-        try:
-            cursor.execute(query, (user_id,))
-            webs = cursor.fetchall()
+        webs = db.select(query, (user_id,), all=True)
+        if webs:
             return webs
-        except Exception as ex:
-            print(ex)
-        finally:
-            close_db()
+        return []
 
-    def add(self, web: Website) -> None:
-        connection, cursor = get_db()
+    def add(self, web: Website, db: QueryManager) -> None:
         query = '''INSERT INTO Websites(user_id, web_name, web_email, web_pass,
         nota, web_username) VALUES(%s, %s, %s, %s, %s, %s)'''
 
-        try:
-            cursor.execute(query, (web.user_id, web.web_name,
-                                   web.web_email, web.web_pass,
-                                   web.nota, web.web_username))
-            connection.commit()
-        except Exception as ex:
-            print(ex)
-        finally:
-            close_db()
+        db.insert(query, (web.user_id, web.web_name,
+                          web.web_email, web.web_pass,
+                          web.nota, web.web_username))
 
     def update(self, web: Website, web_id: int) -> None:
         connection, cursor = get_db()
