8b5daec51c8b4ff9919d4f7c6678bbee8f6906e6
Edmartt <47486245+Edmartt@users.noreply.github.com>
2021-09-19 23:52:03 -0500

    now the forms classes has a better structure and the correct error messages.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,15 +1,35 @@
-from wtforms import StringField,SubmitField,PasswordField
-from wtforms.validators import DataRequired,Email,Length,Regexp,EqualTo
+from wtforms import StringField, SubmitField, PasswordField
+from wtforms.validators import DataRequired, Email, Length, Regexp, EqualTo
 from flask_wtf import FlaskForm
 
 
 class Login(FlaskForm):
-    username=StringField('Username',validators=[DataRequired(),Length(1,64)])
-    password=PasswordField('Password',validators=[DataRequired(),Length(8,64)])
-    submit=SubmitField('Iniciar')
+
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(1, 64)])
+
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(8, 64)])
+    submit = SubmitField('Signup')
+
 
 class Register(FlaskForm):
-    username=StringField('Username',validators=[DataRequired(),Length(1,64),Regexp('^[A-Za-z][A-Za-z0-9_.]*$',0,'El nombre de usuario no debe contener caracteres extraños')])
-    email=StringField('Email',validators=[DataRequired(),Length(1,64)])
-    password=PasswordField('Password',validators=[DataRequired(),Length(8,64)])
-    submit=SubmitField('Registrarse')
+
+    username = StringField(
+            'Username', validators=[DataRequired(), Length(1, 64),
+                                    Regexp('^[A-Za-z][A-Za-z0-9_.]*$', 0,
+                                    'Wrong username format')])
+
+    email = StringField(
+            'Email', validators=[DataRequired(), Length(1, 64), Email()])
+
+    password = PasswordField(
+
+            'Password', validators=[DataRequired(), Length(
+                8, 64, message='Password must be 8 characters minimum'),
+                EqualTo('passconfirm', message='Password must match')])
+
+    passconfirm = PasswordField(
+            'Confirm Password', validators=[DataRequired()])
+
+    submit = SubmitField('Signup')
